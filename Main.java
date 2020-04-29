327a1e3014b60c32333ce472c1e7b5aa151bae36
kostasch <sdi1600195@di.uoa.gr>
2020-04-29 16:57:38 +0300

    Everything seems ok.
    Signed-off-by: kostasch <sdi1600195@di.uoa.gr>

--- Main.java
+++ Main.java
@@ -11,59 +11,55 @@ import SemanticAnalyzer.*;
 
 class Main {
     public static void main(String[] args) {
-        if (args.length != 1) {
+        if (args.length == 0) {
             System.err.println("Usage: java Driver <inputFile>");
             System.exit(-1);
         }
         FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(args[0]);
-            MiniJavaParser mjparser = new MiniJavaParser(fis);
-            Goal root = mjparser.Goal();
-            System.err.println("Program parsed successfully");
-
-            ClassDefinitions classDefs = new ClassDefinitions();
-            root.accept(classDefs, null);
-
-            SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(classDefs);
-            boolean failed = classDefs.getErrorMessages().size() > 0;
-
+        for (int i = 0 ; i < args.length ; i++) {
             try {
-                root.accept(semanticAnalyzer, null);
-            }
-            catch (RuntimeException re) {
-                classDefs.getErrorMessages().add(re.getMessage());
-                failed = true;
-            }
+                fis = new FileInputStream(args[i]);
+                MiniJavaParser mjparser = new MiniJavaParser(fis);
+                Goal root = mjparser.Goal();
+                System.err.println("Program parsed successfully");
 
-            if (!failed) {
-                System.err.println("Semantic analysis passed successfully");
+                ClassDefinitions classDefs = new ClassDefinitions();
+                root.accept(classDefs, null);
 
-            }
-            else {
-                printErrors(classDefs.getErrorMessages());
-                System.err.println("Semantic analysis failed");
-            }
+                SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(classDefs);
+                boolean failed = classDefs.getErrorMessages().size() > 0;
 
-            System.out.println("\nPrinting offsets\n");
-            classDefs.printOffsets();
-        }
-        catch (ParseException ex) {
-            System.err.println(ex.getMessage());
-        }
-        catch (FileNotFoundException ex) {
-            System.err.println(ex.getMessage());
-        }
-        finally {
-            try {
-                if (fis != null) {
-                    fis.close();
+                try {
+                    root.accept(semanticAnalyzer, null);
+                } catch (RuntimeException re) {
+                    classDefs.getErrorMessages().add(re.getMessage());
+                    failed = true;
+                }
+
+                if (!failed) {
+                    System.err.println("Semantic analysis passed successfully");
+
+                } else {
+                    printErrors(classDefs.getErrorMessages());
+                    System.err.println("Semantic analysis failed");
+                }
+
+                System.out.println("\nPrinting offsets\n");
+                classDefs.printOffsets();
+            } catch (ParseException ex) {
+                System.err.println(ex.getMessage());
+            } catch (FileNotFoundException ex) {
+                System.err.println(ex.getMessage());
+            } finally {
+                try {
+                    if (fis != null) {
+                        fis.close();
+                    }
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
                 }
             }
-            catch(IOException ex) {
-                System.err.println(ex.getMessage());
-            }
-        }
+         }
     }
 
     public static void printErrors(List<String> errorMessages) {
